cbe74b6a0048a35941996ff0adaff6e2018e11d7
Nate Binkley <glydout@gmail.com>
2026-05-07 14:48:56 -0400

    [Debug] Added missing @Final tag

--- src/main/java/jsequitur/generation/mixin/OceanBorderMixin.java
+++ src/main/java/jsequitur/generation/mixin/OceanBorderMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.core.world.World;
 import net.minecraft.core.world.chunk.Chunk;
 import net.minecraft.core.world.generate.chunk.ChunkGeneratorResult;
 import net.minecraft.core.world.generate.chunk.perlin.overworld.SurfaceGeneratorOverworld;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(SurfaceGeneratorOverworld.class)
 public class OceanBorderMixin {
 
+	@Final
 	@Shadow
 	private World world;
 
