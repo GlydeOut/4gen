3695dda79a8ae9b52796d21c4cd58a0b9cca9942
Nate Binkley <glydout@gmail.com>
2026-05-07 13:19:13 -0400

    [Implementation] Added universal world border and Nether wall

--- src/main/java/jsequitur/generation/FourGen.java
+++ src/main/java/jsequitur/generation/FourGen.java
@@ -1,4 +1,4 @@
-package turniplabs.examplemod;
+package jsequitur.generation;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -6,12 +6,12 @@ import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
-	public static final String MOD_ID = "examplemod";
+public class FourGen implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+	public static final String MOD_ID = "4gen";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
-		LOGGER.info("ExampleMod initialized.");
+		LOGGER.info("4gen initialized.");
 	}
 
 	@Override
